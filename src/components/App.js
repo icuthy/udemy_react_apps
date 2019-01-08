77418e625799e31abbb9c661df8d327afa89c585
icuthy <icuthy@gmail.com>
2019-01-08 14:57:22 -0800

    Method 1 of Getting response from asynchronous request: using .then and arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
+  state = { images: [] };
 
   onSearchSubmit(term) {
     axios.get('https://api.unsplash.com/search/photos', {
@@ -11,7 +12,9 @@ class App extends React.Component {
       headers: {
         Authorization: 'Client-ID 26f1d9b70bf982834a7201fbe35c7ce31778d119cb22c7922e5d8098bf273c3f'
       }
-    });
+    }).then((response) => {
+      console.log(response.data.results);
+    })
   }
   render() {
     return (
