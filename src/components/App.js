0db870d6f33fd3874339fc4853fb3dd5e7c14ce3
icuthy <icuthy@gmail.com>
2019-01-08 15:24:17 -0800

    Bind value of this with arrow function to onSearchSubmit to fix this.setState context

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     const response = await axios
       .get('https://api.unsplash.com/search/photos', {
         params: { query: term },
@@ -14,12 +14,13 @@ class App extends React.Component {
           Authorization: 'Client-ID 26f1d9b70bf982834a7201fbe35c7ce31778d119cb22c7922e5d8098bf273c3f'
         }
       });
-    console.log(response.data.results);
+    this.setState({ images: response.data.results });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '20px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
