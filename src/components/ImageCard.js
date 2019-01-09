b6509fae8196d0e2de39e4fcb360dca24ae35371
icuthy <icuthy@gmail.com>
2019-01-09 14:10:39 -0800

    Add react Ref to ImageCard img with constructor

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -8,7 +8,7 @@ class ImageCard extends React.Component {
 
     // don't need to put ref in state since data is not going to change
     componentDidMount() {
-
+        // when console.log(this.imageRef.current.clientHeight) produces 0 in BhxBrowser, cos console.log runs before images are downloaded from unsplash url
         console.log(this.imageRef);
     }
     render() {
