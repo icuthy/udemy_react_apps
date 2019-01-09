e8fe063994b6467626ad8f91cdc2d53fc01a7ef9
icuthy <icuthy@gmail.com>
2019-01-09 14:42:03 -0800

    Set up dynamic height on containing divs depending on image height using state

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,24 +3,32 @@ import React from 'react';
 class ImageCard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { spans: 0 };
         this.imageRef = React.createRef();
     }
 
-    // Refactored with call back on image load so we get access to image height before images are ??downloaded from Unsplash
+    // Refactored with call back on image load so we get access to image height before images are downloaded from Unsplash
     componentDidMount() {
         this.imageRef.current.addEventListener('load', this.setSpans);
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight)
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans });
     };
 
     render() {
         // props destructuring
         const { urls, description } = this.props.image;
         return (
-            <div>
-                <img ref={this.imageRef} src={urls.regular} alt={description} />
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }} >
+                <img
+                    ref={this.imageRef}
+                    src={urls.regular}
+                    alt={description}
+                />
             </div>
         );
     }
