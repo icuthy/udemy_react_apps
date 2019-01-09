d8c0084cf7300a979ff3656f25cce0598a2d0565
icuthy <icuthy@gmail.com>
2019-01-09 14:09:53 -0800

    Add react Ref to ImageCard img with constructor

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+
+    // don't need to put ref in state since data is not going to change
+    componentDidMount() {
+
+        console.log(this.imageRef);
+    }
     render() {
         // props destructuring
         const { urls, description } = this.props.image;
         return (
             <div>
-                <img src={urls.regular} alt={description} />
+                <img ref={this.imageRef} src={urls.regular} alt={description} />
             </div>
         );
     }
